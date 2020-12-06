12f33c70535b91e8747a48636a0f1ffa92eaf865
Max sjolinder <max.sjolinder@gmail.com>
2020-12-06 12:54:42 +0100

    adjusted nuget deploy path

--- upload_nuget.sh
+++ upload_nuget.sh
@@ -2,4 +2,4 @@
 
 mkdir -p ./nuget
 dotnet pack ./NorthernLight.NasdaqNordic/NorthernLight.NasdaqNordic.csproj -c Release -o ./nuget
-dotnet nuget push ./NorthernLight.NasdaqNordic/nuget/NorthernLight.NasdaqNordic.*.nupkg -s https://api.nuget.org/v3/index.json -k $NUGET_API_KEY
+dotnet nuget push ./nuget/NorthernLight.NasdaqNordic.*.nupkg -s https://api.nuget.org/v3/index.json -k $NUGET_API_KEY
